6f14a17371b4764a60d5de51e23b0bab867878d4
MRNCallero <mariano.callero@cencosud.com>
2022-09-30 16:25:58 -0300

    extraer controlando moviemiento

--- test/extraer.test.js
+++ test/extraer.test.js
@@ -3,8 +3,11 @@ describe('PUT /extraer/{id}', () => {
     test('Debe devolver un código de estado 200', async () => {
 
         const data = { id: 1, monto : 100};
+        const usuinicial = db.Usuario.findPk(1);
         const { statusCode, body } = await request(app).put('/extraer/1').send(data);
+        const usufinal = db.Usuario.findPk(1);
         expect(statusCode).toBe(200);
+        expect(usuinicial.saldo - data.monto).toEqual(usufinal.sadlo);
      })
 
      test('Debe devolver un código de estado 404', async () => {
