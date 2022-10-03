e5e9764bfb4ece1a9b0ffd306caf4c74af3897fa
andrescorchs <andrescorchs@gmail.com>
2022-10-03 09:52:23 -0300

    start of test

---
+++ test/registro.test.js
@@ -0,0 +1,43 @@
+import { productsDB } from '../src/data/productsDB';
+import { getProductByCategory, getProductById } from '../src/products';
+
+describe('Registro de usuarios', () => {
+
+   test('Correcto registro de usuario', () => {
+
+      const id = 1;
+      // {firstname, lastname, birthdate, email, password, repitePassword} = 
+
+      const product = getProductById(id);
+
+      // expect(product).toEqual({ id: 1, name: 'Fideos', category: 'alimentos' });
+      // expect(product).not.toBeFalsy();
+      expect(product).toEqual(productsDB.find(product => product.id === id));
+      expect(product).toBeTruthy();
+
+   });
+
+   test('getProductById tiene que retornar undefined', () => {
+
+      const id = 10;
+
+      const product = getProductById(id);
+
+      expect(product).toBeFalsy();
+   });
+
+   test('getProductByCategory tiene que retornar un array de productos', () => {
+
+      const category = 'alimentos';
+
+      const product = getProductByCategory(category);
+
+      expect(product.length).toBe(3);
+      expect(product).toEqual(expect.arrayContaining([
+         expect.objectContaining({
+            id: expect.any(Number)
+         })
+      ]))
+   });
+
+});
